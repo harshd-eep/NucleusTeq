a7cefcde3e9399f0d937efa4f0df72d71ff567d1
harshd-eep <55032343+harshd-eep@users.noreply.github.com>
2023-03-24 18:08:44 +0530

    Add files via upload

---
+++ Divide.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Divide {
+    public static void main(String args[]) {
+        int a,b;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int c=a/b;
+        System.out.println("a/b="+c);
+    }
+}
+
+
